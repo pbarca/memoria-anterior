535d2ecc94f2f6b85b5f838ebd6218460965e982
Paulo Coto <Paulo Coto@DESKTOP-QBHA61I>
2019-10-01 20:19:41 +0100

    sleep

--- memoria4/Form1.vb
+++ memoria4/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Dim Matriz(12) As Integer
+    Dim Ajogar = 1
+    Dim Anterior As Integer
     Dim Quadros() As PictureBox
     Private Sub Inicializa()
         Dim i As Integer
@@ -26,24 +28,35 @@
             Case 4 : fig.BackgroundImage = My.Resources.leo
             Case 5 : fig.BackgroundImage = My.Resources.libra
         End Select
-
         Quadros(quadro).BackgroundImage = fig.BackgroundImage
     End Sub
     Private Sub Clicar(sender As Object, e As EventArgs) Handles P1.Click, P2.Click, P4.Click, P3.Click, P9.Click, P8.Click, P7.Click, P6.Click, P5.Click, P12.Click, P11.Click, P10.Click
+        Dim quadro
         Select Case sender.name
-            Case "P1" : Imagem(0)
-            Case "P2" : Imagem(1)
-            Case "P3" : Imagem(2)
-            Case "P4" : Imagem(3)
-            Case "P5" : Imagem(4)
-            Case "P6" : Imagem(5)
-            Case "P7" : Imagem(6)
-            Case "P8" : Imagem(7)
-            Case "P9" : Imagem(8)
-            Case "P10" : Imagem(9)
-            Case "P11" : Imagem(10)
-            Case "P12" : Imagem(11)
+            Case "P1" : quadro = 0
+            Case "P2" : quadro = 1
+            Case "P3" : quadro = 2
+            Case "P4" : quadro = 3
+            Case "P5" : quadro = 4
+            Case "P6" : quadro = 5
+            Case "P7" : quadro = 6
+            Case "P8" : quadro = 7
+            Case "P9" : quadro = 8
+            Case "P10" : quadro = 9
+            Case "P11" : quadro = 10
+            Case "P12" : quadro = 11
         End Select
+        Imagem(quadro)
+        If Ajogar = 1 Then : Anterior = quadro
+        ElseIf Matriz(Anterior) <> Matriz(quadro) Then
+            Threading.Thread.Sleep(1500)
+            Quadros(quadro).BackgroundImage = My.Resources.Nada
+            Quadros(Anterior).BackgroundImage = My.Resources.Nada
+        End If
+
+        If Ajogar = 1 Then : Ajogar = 2
+        Else : Ajogar = 1
+        End If
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
