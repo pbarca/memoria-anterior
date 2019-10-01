07370b8140c199ead10da6b9d5bc446573bd56c4
Paulo Coto <Paulo Coto@DESKTOP-QBHA61I>
2019-10-01 19:53:19 +0100

    Matriz

--- memoria4/Form1.Designer.vb
+++ memoria4/Form1.Designer.vb
@@ -56,9 +56,10 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Harrington", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Salmon
-        Me.Label1.Location = New System.Drawing.Point(204, 9)
+        Me.Label1.Location = New System.Drawing.Point(272, 11)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(392, 56)
+        Me.Label1.Size = New System.Drawing.Size(482, 71)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Jogo da Memória"
         '
@@ -66,9 +67,10 @@ Partial Class Form1
         '
         Me.P12.BackgroundImage = CType(resources.GetObject("P12.BackgroundImage"), System.Drawing.Image)
         Me.P12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P12.Location = New System.Drawing.Point(580, 428)
+        Me.P12.Location = New System.Drawing.Point(773, 527)
+        Me.P12.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P12.Name = "P12"
-        Me.P12.Size = New System.Drawing.Size(128, 128)
+        Me.P12.Size = New System.Drawing.Size(171, 158)
         Me.P12.TabIndex = 12
         Me.P12.TabStop = False
         '
@@ -76,9 +78,10 @@ Partial Class Form1
         '
         Me.P11.BackgroundImage = CType(resources.GetObject("P11.BackgroundImage"), System.Drawing.Image)
         Me.P11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P11.Location = New System.Drawing.Point(417, 428)
+        Me.P11.Location = New System.Drawing.Point(556, 527)
+        Me.P11.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P11.Name = "P11"
-        Me.P11.Size = New System.Drawing.Size(128, 128)
+        Me.P11.Size = New System.Drawing.Size(171, 158)
         Me.P11.TabIndex = 11
         Me.P11.TabStop = False
         '
@@ -86,9 +89,10 @@ Partial Class Form1
         '
         Me.P10.BackgroundImage = CType(resources.GetObject("P10.BackgroundImage"), System.Drawing.Image)
         Me.P10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P10.Location = New System.Drawing.Point(255, 428)
+        Me.P10.Location = New System.Drawing.Point(340, 527)
+        Me.P10.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P10.Name = "P10"
-        Me.P10.Size = New System.Drawing.Size(128, 128)
+        Me.P10.Size = New System.Drawing.Size(171, 158)
         Me.P10.TabIndex = 10
         Me.P10.TabStop = False
         '
@@ -96,9 +100,10 @@ Partial Class Form1
         '
         Me.P9.BackgroundImage = CType(resources.GetObject("P9.BackgroundImage"), System.Drawing.Image)
         Me.P9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P9.Location = New System.Drawing.Point(93, 428)
+        Me.P9.Location = New System.Drawing.Point(124, 527)
+        Me.P9.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P9.Name = "P9"
-        Me.P9.Size = New System.Drawing.Size(128, 128)
+        Me.P9.Size = New System.Drawing.Size(171, 158)
         Me.P9.TabIndex = 9
         Me.P9.TabStop = False
         '
@@ -106,9 +111,10 @@ Partial Class Form1
         '
         Me.P8.BackgroundImage = CType(resources.GetObject("P8.BackgroundImage"), System.Drawing.Image)
         Me.P8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P8.Location = New System.Drawing.Point(580, 265)
+        Me.P8.Location = New System.Drawing.Point(773, 326)
+        Me.P8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P8.Name = "P8"
-        Me.P8.Size = New System.Drawing.Size(128, 128)
+        Me.P8.Size = New System.Drawing.Size(171, 158)
         Me.P8.TabIndex = 8
         Me.P8.TabStop = False
         '
@@ -116,9 +122,10 @@ Partial Class Form1
         '
         Me.P7.BackgroundImage = CType(resources.GetObject("P7.BackgroundImage"), System.Drawing.Image)
         Me.P7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P7.Location = New System.Drawing.Point(417, 265)
+        Me.P7.Location = New System.Drawing.Point(556, 326)
+        Me.P7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P7.Name = "P7"
-        Me.P7.Size = New System.Drawing.Size(128, 128)
+        Me.P7.Size = New System.Drawing.Size(171, 158)
         Me.P7.TabIndex = 7
         Me.P7.TabStop = False
         '
@@ -126,9 +133,10 @@ Partial Class Form1
         '
         Me.P6.BackgroundImage = CType(resources.GetObject("P6.BackgroundImage"), System.Drawing.Image)
         Me.P6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P6.Location = New System.Drawing.Point(255, 265)
+        Me.P6.Location = New System.Drawing.Point(340, 326)
+        Me.P6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P6.Name = "P6"
-        Me.P6.Size = New System.Drawing.Size(128, 128)
+        Me.P6.Size = New System.Drawing.Size(171, 158)
         Me.P6.TabIndex = 6
         Me.P6.TabStop = False
         '
@@ -136,9 +144,10 @@ Partial Class Form1
         '
         Me.P5.BackgroundImage = CType(resources.GetObject("P5.BackgroundImage"), System.Drawing.Image)
         Me.P5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P5.Location = New System.Drawing.Point(93, 265)
+        Me.P5.Location = New System.Drawing.Point(124, 326)
+        Me.P5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P5.Name = "P5"
-        Me.P5.Size = New System.Drawing.Size(128, 128)
+        Me.P5.Size = New System.Drawing.Size(171, 158)
         Me.P5.TabIndex = 5
         Me.P5.TabStop = False
         '
@@ -146,9 +155,10 @@ Partial Class Form1
         '
         Me.P4.BackgroundImage = CType(resources.GetObject("P4.BackgroundImage"), System.Drawing.Image)
         Me.P4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P4.Location = New System.Drawing.Point(580, 107)
+        Me.P4.Location = New System.Drawing.Point(773, 132)
+        Me.P4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P4.Name = "P4"
-        Me.P4.Size = New System.Drawing.Size(128, 128)
+        Me.P4.Size = New System.Drawing.Size(171, 158)
         Me.P4.TabIndex = 4
         Me.P4.TabStop = False
         '
@@ -156,9 +166,10 @@ Partial Class Form1
         '
         Me.P3.BackgroundImage = CType(resources.GetObject("P3.BackgroundImage"), System.Drawing.Image)
         Me.P3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P3.Location = New System.Drawing.Point(417, 107)
+        Me.P3.Location = New System.Drawing.Point(556, 132)
+        Me.P3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P3.Name = "P3"
-        Me.P3.Size = New System.Drawing.Size(128, 128)
+        Me.P3.Size = New System.Drawing.Size(171, 158)
         Me.P3.TabIndex = 3
         Me.P3.TabStop = False
         '
@@ -166,9 +177,10 @@ Partial Class Form1
         '
         Me.P2.BackgroundImage = CType(resources.GetObject("P2.BackgroundImage"), System.Drawing.Image)
         Me.P2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P2.Location = New System.Drawing.Point(255, 107)
+        Me.P2.Location = New System.Drawing.Point(340, 132)
+        Me.P2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P2.Name = "P2"
-        Me.P2.Size = New System.Drawing.Size(128, 128)
+        Me.P2.Size = New System.Drawing.Size(171, 158)
         Me.P2.TabIndex = 2
         Me.P2.TabStop = False
         '
@@ -176,27 +188,29 @@ Partial Class Form1
         '
         Me.P1.BackgroundImage = CType(resources.GetObject("P1.BackgroundImage"), System.Drawing.Image)
         Me.P1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.P1.Location = New System.Drawing.Point(93, 107)
+        Me.P1.Location = New System.Drawing.Point(124, 132)
+        Me.P1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.P1.Name = "P1"
-        Me.P1.Size = New System.Drawing.Size(128, 128)
+        Me.P1.Size = New System.Drawing.Size(171, 158)
         Me.P1.TabIndex = 1
         Me.P1.TabStop = False
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Jokerman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(312, 587)
+        Me.Button1.Location = New System.Drawing.Point(416, 722)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(171, 39)
+        Me.Button1.Size = New System.Drawing.Size(228, 48)
         Me.Button1.TabIndex = 13
         Me.Button1.Text = "Novo Jogo"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 655)
+        Me.ClientSize = New System.Drawing.Size(1067, 806)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.P12)
         Me.Controls.Add(Me.P11)
@@ -211,6 +225,7 @@ Partial Class Form1
         Me.Controls.Add(Me.P2)
         Me.Controls.Add(Me.P1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.P12, System.ComponentModel.ISupportInitialize).EndInit()
--- memoria4/Form1.vb
+++ memoria4/Form1.vb
@@ -1,29 +1,57 @@
 ﻿Public Class Form1
-    Private Sub Clicar(sender As Object, e As EventArgs) Handles P1.Click, P2.Click, P4.Click, P3.Click, P9.Click, P8.Click, P7.Click, P6.Click, P5.Click, P12.Click, P11.Click, P10.Click
+    Dim Matriz(12) As Integer
+    Dim Quadros() As PictureBox
+    Private Sub Inicializa()
+        Dim i As Integer
+        For i = 0 To 11
+            Matriz(i) = 0
+            Quadros(i).BackgroundImage = My.Resources.Nada
+        Next
+
+        For par = 0 To 5
+            For x = 0 To 1
+                Do : i = Int(Rnd() * 12)
+                Loop While Matriz(i) > 0
+                Matriz(i) = par
+            Next
+        Next
+    End Sub
+    Private Sub Imagem(quadro)
         Dim fig As New PictureBox
-        Dim i = Int(Rnd() * 6)
-        Select Case i
-            Case 0 : fig.Image = My.Resources.aries
-            Case 1 : fig.Image = My.Resources.aquarius
-            Case 2 : fig.Image = My.Resources.capricornus
-            Case 3 : fig.Image = My.Resources.gemini
-            Case 4 : fig.Image = My.Resources.leo
-            Case 5 : fig.Image = My.Resources.libra
+        Select Case Matriz(quadro)
+            Case 0 : fig.BackgroundImage = My.Resources.aries
+            Case 1 : fig.BackgroundImage = My.Resources.aquarius
+            Case 2 : fig.BackgroundImage = My.Resources.pisces
+            Case 3 : fig.BackgroundImage = My.Resources.gemini
+            Case 4 : fig.BackgroundImage = My.Resources.leo
+            Case 5 : fig.BackgroundImage = My.Resources.libra
         End Select
 
+        Quadros(quadro).BackgroundImage = fig.BackgroundImage
+    End Sub
+    Private Sub Clicar(sender As Object, e As EventArgs) Handles P1.Click, P2.Click, P4.Click, P3.Click, P9.Click, P8.Click, P7.Click, P6.Click, P5.Click, P12.Click, P11.Click, P10.Click
         Select Case sender.name
-            Case "P1" : P1.BackgroundImage = fig.Image
-            Case "P2" : P2.BackgroundImage = fig.Image
-            Case "P3" : P3.BackgroundImage = fig.Image
-            Case "P4" : P4.BackgroundImage = fig.Image
-            Case "P5" : P5.BackgroundImage = fig.Image
-            Case "P6" : P6.BackgroundImage = fig.Image
-            Case "P7" : P7.BackgroundImage = fig.Image
-            Case "P8" : P8.BackgroundImage = fig.Image
-            Case "P9" : P9.BackgroundImage = fig.Image
-            Case "P10" : P10.BackgroundImage = fig.Image
-            Case "P11" : P11.BackgroundImage = fig.Image
-            Case "P12" : P12.BackgroundImage = fig.Image
+            Case "P1" : Imagem(0)
+            Case "P2" : Imagem(1)
+            Case "P3" : Imagem(2)
+            Case "P4" : Imagem(3)
+            Case "P5" : Imagem(4)
+            Case "P6" : Imagem(5)
+            Case "P7" : Imagem(6)
+            Case "P8" : Imagem(7)
+            Case "P9" : Imagem(8)
+            Case "P10" : Imagem(9)
+            Case "P11" : Imagem(10)
+            Case "P12" : Imagem(11)
         End Select
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Inicializa()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Quadros = {P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12}
+        Call Inicializa()
     End Sub
 End Class
